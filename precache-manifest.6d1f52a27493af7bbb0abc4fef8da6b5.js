4d4dd27c2667690232e2d2a9ed0390368043151f
JaeJoong Lee <approach0218@gmail.com>
2020-02-17 16:08:08 +0900

    Updates

---
+++ precache-manifest.6d1f52a27493af7bbb0abc4fef8da6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802db37a7002ae2e5f4d6acf5b9b83f0",
+    "url": "/Movice_App/index.html"
+  },
+  {
+    "revision": "437ef13f3c29eafec05c",
+    "url": "/Movice_App/static/css/main.821f2d0e.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/Movice_App/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Movice_App/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437ef13f3c29eafec05c",
+    "url": "/Movice_App/static/js/main.19f7d471.chunk.js"
+  },
+  {
+    "revision": "3118c9e0a299c7fb3963",
+    "url": "/Movice_App/static/js/runtime-main.ac9f4f3b.js"
+  }
+]);
